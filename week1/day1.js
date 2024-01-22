5eea4c877587ceadf321fb297fc762ecdc7bb7e0
Peter Heri <heripeter725@gmail.com>
2024-01-22 13:11:36 +0300

    week 1 js concepts

---
+++ week1/day1.js
@@ -0,0 +1,57 @@
+/**
+ * @var
+* var can be reassign and redeclare
+ var name = " Dan"
+ var ]name = "James"
+
+@let
+You can reassign
+
+let name = "James"
+name = "Dan"
+
+@const
+ neither reassign or redeclare
+
+ @Strings declare characters  e.g "Dan" "1" must be in quotation marks
+ @Numbers declare numbers e.g 1, 1.5
+ @Boolean declare values either trie or false
+ @Null  absence of a value, e.g., null.
+@Undefined  Rvariable that has been declared but not assigned a value, e.g., undefined.
+ * 
+
+
+@listens. array represents list [1, 2, 3]
+@  Represents key-value pairs, e.g., { name: "John", age: 30 }.
+ */
+
+/** Mathematics 
+ * @Modulus picks the reminder
+ * 
+ * 
+ * 
+ */
+
+//Revenue
+//Expense
+//Cost of goods
+
+const costofsock = 50
+const costofbag = 50
+
+const costofbatch = (costofsock * 3)
+const sellingprice = 1000
+const revenue = (sellingprice * 30) * 70/100
+
+const costofsoldsocks = costofsock * 90
+const costofsoldbags =  costofsoldsocks * 30
+
+const profit = 1000 - (500 - 200)
+
+const numberofsocks = 100
+const remainingsocks = 10
+
+console.log(profit)
+
+
+
